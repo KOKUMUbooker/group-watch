ffa3892a8c7293c3771f3497db9c3ba72bf5b82d
KOKUMUbooker <bookerochieng20@gmail.com>
2026-01-24 08:32:04 +0300

    Add MovieDBContext

---
+++ Models/MovieDBContext.cs
@@ -0,0 +1,15 @@
+using  Microsoft.EntityFrameworkCore;
+
+namespace MovieManager.Models;
+
+public class MovieDbContext(DbContextOptions<MovieDbContext> options) : DbContext(options)
+{
+    public DbSet<Movie> Movies => Set<Movie>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("app");
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MovieDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
