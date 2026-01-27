d0a2046467b5b0fd48a6c6308148b9793153666e
KOKUMUbooker <bookerochieng20@gmail.com>
2026-01-27 14:58:16 +0300

    Add login page

---
+++ UI/src/lib/components/login/login-form.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+  import {
+  	Field,
+  	FieldDescription,
+  	FieldGroup,
+  	FieldLabel
+  } from "$lib/components/ui/field/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+  import { cn, type WithElementRef } from "$lib/utils.js";
+  import type { HTMLFormAttributes } from "svelte/elements";
+  let {
+    ref = $bindable(null),
+    class: className,
+    ...restProps
+  }: WithElementRef<HTMLFormAttributes> = $props();
+  const id = $props.id();
+</script>
+<form class={cn("flex flex-col gap-6", className)} bind:this={ref} {...restProps}>
+  <FieldGroup>
+    <div class="flex flex-col items-center gap-1 text-center">
+      <h1 class="text-2xl font-bold">Login to your account</h1>
+      <p class="text-muted-foreground text-sm text-balance">
+        Enter your email below to login to your account
+      </p>
+    </div>
+    <Field>
+      <FieldLabel for="email-{id}">Email</FieldLabel>
+      <Input id="email-{id}" type="email" placeholder="m@example.com" required />
+    </Field>
+    <Field>
+      <div class="flex items-center">
+        <FieldLabel for="password-{id}">Password</FieldLabel>
+        <a href="##" class="ms-auto text-sm underline-offset-4 hover:underline">
+          Forgot your password?
+        </a>
+      </div>
+      <Input id="password-{id}" type="password" required />
+    </Field>
+    <Field>
+      <Button type="submit">Login</Button>
+    </Field>
+    <Field>
+      <FieldDescription class="text-center">
+        Don't have an account?
+        <a href="##" class="underline underline-offset-4">Sign up</a>
+      </FieldDescription>
+    </Field>
+  </FieldGroup>
+</form>
---
+++ UI/src/routes/login/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import LoginForm from "@/components/login/login-form.svelte";
+  import mvImg from "$lib/assets/movie.jpg"
+</script>
+<div class="grid min-h-svh lg:grid-cols-2">
+  <div class="flex flex-col gap-4 p-6 md:p-10">
+
+    <div class="flex flex-1 items-center justify-center">
+      <div class="w-full max-w-xs">
+        <LoginForm />
+      </div>
+    </div>
+  </div>
+  <div class="bg-muted relative hidden lg:block">
+    <img
+      src={mvImg}
+      alt="placeholder"
+      class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.8] dark:grayscale"
+    />
+  </div>
+</div>
