a6dfeb9e5b9a1c442fd1fa75999f07151d197432
KOKUMUbooker <bookerochieng20@gmail.com>
2026-02-10 18:49:29 +0300

    Remove unused model files

--- Server/Models/Cast.cs
+++
@@ -1,9 +0,0 @@
-namespace MovieManager.Models;
-
-public class Cast : EntityBase {
-    public required string Name {get; set;}
-    public required string ImageUrl{get; set;}
-
-    // Navigational properties
-    public List<MovieCast> MovieCasts { get; set; } = new List<MovieCast>();
-}
--- Server/Models/User.cs
+++ Server/Models/User.cs
@@ -7,19 +7,16 @@ public class User : EntityBase
     [Required(ErrorMessage = "Email is required.")]
     [EmailAddress(ErrorMessage = "Invalid email address format.")]
     [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters.")]
-    public required string Email { get; set; }
+    public  string Email { get; set; }
 
     [MinLength(3, ErrorMessage = "Full name must be at least 3 characters long")]
-    public required string FullName { get; set; }
+    public string FullName { get; set; }
 
     [Required]
     public Guid RoleId { get; set; }
 
     [Required]
-    public required string PasswordHash { get; set; }
-
-    // Number of movies added by the user
-    public int Movies {get; set;} = 0;
+    public string PasswordHash { get; set; }
 
     public bool EmailVerified { get; set; } = false;
     public string? EmailVerificationToken { get; set; }
